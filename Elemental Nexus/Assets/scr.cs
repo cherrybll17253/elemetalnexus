a267a152192fefd533309d5da301d15705df423c
swjkyr <130287258+swjkyr@users.noreply.github.com>
2024-08-31 23:12:27 +0900

    sfsd

--- Elemental Nexus/Assets/scr.cs
+++ Elemental Nexus/Assets/scr.cs
@@ -7,7 +7,7 @@ public class scr : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        sdgdsgss
     }
 
     // Update is called once per frame
